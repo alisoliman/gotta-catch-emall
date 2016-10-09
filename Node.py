007effedef2e0ca42a7ecb8bb1480965f5843d19
azamzamy <a.zamzamy@hotmail.com>
2016-10-09 22:46:33 +0200

    new files

---
+++ Node.py
@@ -0,0 +1,12 @@
+class Node():
+
+    def __init__(self, state, parent, op, depth, pathCost):
+        self.state = state
+        self.parent = parent
+        self.operation = op
+        self.depth = depth
+        self.pathCost = pathCost
+
+
+
+
---
+++ SearchProblem.py
@@ -0,0 +1,7 @@
+class SearchEngine():
+    def __init__(self, maze):
+        self.maze = maze
+
+    
+
+
