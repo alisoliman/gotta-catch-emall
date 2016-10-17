8c5e84e9d2c1089dcbaff580d7d8de689edbe42a
azamzamy <a.zamzamy@hotmail.com>
2016-10-17 22:31:04 +0200

    modified Cell.py

--- Cell.py
+++ Cell.py
@@ -1,10 +1,2 @@
 class Cell():
 
-    def __init__(self, location, parent, op, depth, pathCost):
-        self.state = state
-        self.parent = parent
-        self.operation = op
-        self.depth = depth
-        self.pathCost = pathCost
-
-
